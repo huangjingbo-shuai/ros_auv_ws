0e35d7e6acbf6a2a87c4a031caf96e45d6f63470
yunxia <2226038143@qq.com>
2025-03-14 22:41:30 +0800

    update1

--- build/auv_run/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yunxia/ros_auv_ws/src/auv_run
-# Build directory: /home/yunxia/ros_auv_ws/build/auv_run
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/yunxia/ros_auv_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/yunxia/ros_auv_ws/devel;/home/yunxia/ros_boat_ws/devel;/opt/ros/noetic'.split(';') if '/home/yunxia/ros_auv_ws/devel;/home/yunxia/ros_boat_ws/devel;/opt/ros/noetic' != '' else []
